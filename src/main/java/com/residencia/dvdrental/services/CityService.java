07a6a77c05e65ea539dd8fddd7e8513b71afb563
unknown <deborasouza@id.uff.br>
2021-06-03 12:20:01 -0300

    teste git

---
+++ src/main/java/com/residencia/dvdrental/services/CityService.java
@@ -0,0 +1,42 @@
+package com.residencia.dvdrental.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.residencia.dvdrental.entities.City;
+import com.residencia.dvdrental.repositories.CityRepository;
+
+@Service
+public class CityService {
+    
+	@Autowired
+	public CityRepository cityRepository;
+	
+	public City findById(Integer id) {
+		return cityRepository.findById(id).get();
+	}
+
+	public List<City> findAll() {
+		return cityRepository.findAll();
+	}
+
+	public Long count() {
+		return cityRepository.count();
+	}
+
+	public City save(City city) {
+		City newCity = cityRepository.save(city);
+		return newCity;
+	}
+
+	public City update(City city, Integer id) {
+		city.setCity_id(id);
+		return cityRepository.save(city);
+	}
+
+	public void delete (Integer id) {
+		cityRepository.deleteById(id);
+	}
+}
